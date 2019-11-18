dffd7cadae582d769502e79dd3a95f5c0ad74d34
iqbalmmm <iqbal.itang@gmail.com>
2019-11-18 10:33:08 +0700

    Updates

---
+++ precache-manifest.34562fbea62837978ba558248fd00193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ebd67e18c5411ac759d24c41b609f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2e90e5c42b9d88afc65",
+    "url": "/static/css/main.6eee103d.chunk.css"
+  },
+  {
+    "revision": "3c10fff2b779642900d1",
+    "url": "/static/js/2.24e9b854.chunk.js"
+  },
+  {
+    "revision": "f2e90e5c42b9d88afc65",
+    "url": "/static/js/main.6400f572.chunk.js"
+  },
+  {
+    "revision": "ad645e6c8658e5fb5d05",
+    "url": "/static/js/runtime-main.18f0efaa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
